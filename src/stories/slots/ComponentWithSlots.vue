190f9f03d753284e0beba4fe03ae7fc3f4181202
chakAs3 <chakir.qatab@rhinos-me.com>
2023-06-03 22:37:43 +0400

    slots story test

--- src/stories/slots/ComponentWithSlots.vue
+++ src/stories/slots/ComponentWithSlots.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   label: String,
 });
 </script>
 <template>
-  <div>
+  <div >
     <h1>{{ label }}</h1>
-    <header data-testid="header-slot">
-      <slot name="header" title="Header title from the slot"></slot>
-    </header>
-    <main data-testid="default-slot">
-      <slot></slot>
-    </main>
-    <footer data-testid="footer-slot">
-      <slot name="footer"></slot>
-    </footer>
+    <div style="background-color: antiquewhite;border:solid brown 1px ;padding: 10px;">
+      <h5>SLOTS :</h5>
+      <header data-testid="header-slot">
+        <slot name="header" :title="'Header '+ label +' title from the slot'"></slot>
+      </header>
+      <main data-testid="default-slot">
+        <slot></slot>
+      </main>
+      <footer data-testid="footer-slot">
+        <slot name="footer"></slot>
+      </footer>
+    </div>
   </div>
 </template>
